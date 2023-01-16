6f1bc7ee6ef512dfab05aeec6c1f4562056afe18
maina kimani <me@mainakimani.com>
2023-01-16 17:39:05 +0300

    Note model added

--- notes_api/models.py
+++ notes_api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+#create Note Table in the database together with the attributes
+class Note(models.Model):
+    title = models.CharField(max_length=150)
+    body = models.TextField(null=False, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.title[0:50]
