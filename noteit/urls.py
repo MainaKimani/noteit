b1318023539d19817fd7aa3f7613c1f2fe5184eb
maina kimani <me@mainakimani.com>
2023-01-17 23:57:39 +0300

    notes-api routes included

--- noteit/urls.py
+++ noteit/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('notes_api.urls')),
+    path('api/', include('notes_api.urls')),
 ]
